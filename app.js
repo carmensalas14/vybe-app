55917826589a4dfe229e56e947f399cf56c0b05a
EC2 Default User <ec2-user@ip-172-31-24-236.us-east-2.compute.internal>
2020-03-02 19:31:35 +0000

    added file to recieve user playlists

---
+++ app.js
@@ -0,0 +1,18 @@
+const unique = require('uniq');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+// credentials are optional
+const spotifyApi = new SpotifyWebApi({
+    clientId: client_id,
+    clientSecret: clientSecret,
+    redirectUri: redirectUri
+});
+
+
+spotifyApi.setAccessToken(token);
+spotifyApi.getUserPlaylists('user_id')
+    .then(function (data) {
+        console.log('User playlists', data);
+    }, function (err) {
+        console.error(err);
+    });
