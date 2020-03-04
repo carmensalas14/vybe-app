bfbc255eb4f1fe701c45ac38ab287c8a9535ae04
EC2 Default User <ec2-user@ip-172-31-18-94.us-east-2.compute.internal>
2020-03-04 22:57:30 +0000

    add track names and artist(s)

--- app.js
+++ app.js
@@ -1,26 +1,52 @@
-const SpotifyWebApi = require('spotify-web-api-node');
-
 const client_id = `325321fbe95244a79af7e14e52867182`
 const clientSecret = `YTBmNGJlZTlhYTEyNDFhNTkxNmRhYWZkN2I3YTFlZjQ=`
 const redirectUri = `https%3A%2F%2Fcarmensalas14.github.io%2Fvybe-app%2F`
-const token = `BQCzUe1yL9ccIAp8cLtt_DOm_Z1Vrzt7uIpzvE4hvECT3owIz79P7kxbbOMHftBHGXfGAA3TkV7Ck0Q9ClcmcsgBuMCReyAhfujQScVwBC6oZQj7XltAil7Wevbd2WN5kPANnHW2`
+const token = `BQBbQPhNvzhaWLz3fV_Gh6W9Ur3GxxC8PCrX4xi0iMVvkqPggUMI6PeE6XSozPhqahh_vaIJ9FVlqRN0JYXzKDc4h_fiPUlGIw0Buf4O9PFstyNCXPb5haXg_8xRPc4OSgTwbqghaA5LVl3OV7hU-SCMHBMQ6eLpeG8`
 const implicitAuthorization = `https://accounts.spotify.com/authorize?client_id=325321fbe95244a79af7e14e52867182&redirect_uri=https%3A%2F%2Fcarmensalas14.github.io%2Fvybe-app%2F&response_type=token&&scope=user-read-private%20user-library-read`
-const user_id = `5n770f3k69i7nm3s50m32iu7r`
-// credentials are optional
-const spotifyApi = new SpotifyWebApi({
-    clientId: client_id,
-    clientSecret: clientSecret,
-    redirectUri: redirectUri
-});
+const user_id = `laishaa`
 
-// const params = new URLSearchParams(window.location.hash);
-// const accessToken = params.get("#access_token");
-// console.log(accessToken)
+// const spotifyApi = new SpotifyWebApi({
+//     clientId: client_id,
+//     clientSecret: clientSecret,
+//     redirectUri: redirectUri
+// });
 
-spotifyApi.setAccessToken(token);
-spotifyApi.getUserPlaylists(user_id)
-    .then(function (data) {
-        console.log('User playlists', data);
-    }, function (err) {
-        console.error(err);
+const getUserSavedTracks = async function() {
+    const response = await fetch('https://api.spotify.com/v1/me/tracks?offset=0&limit=50', {
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
     });
+    const json = await response.json();
+    return json
+};
+
+const getTrackItems = async function() {
+    const data = await getUserSavedTracks();
+    const items = await data.items
+    // console.log(items)
+    return items
+}
+
+console.log(getTrackItems())
+
+async function createTrackList() {
+    const tracksArr = await getTrackItems();
+    const trackList = document.createElement('ul')
+    const results = document.getElementById("results")
+
+    function createTrackItem(trackInfo) {
+        const li = document.createElement('li')
+        const artist = trackInfo.track.artists.map(artist => artist.name)
+        li.innerText = `${trackInfo.track.name}, ${artist}`
+        return li
+    }
+
+    for (let i = 0; i < tracksArr.length; i++) {
+        const trackItem = createTrackItem(tracksArr[i])
+        trackList.append(trackItem)
+    }
+    results.appendChild(trackList)
+}
+
+createTrackList();
