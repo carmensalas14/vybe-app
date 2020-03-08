dbd8620261a7a23969e58faa56e0ebfb3bc3cf46
EC2 Default User <ec2-user@ip-172-31-24-236.us-east-2.compute.internal>
2020-03-08 07:32:34 +0000

    added event listeners to energy buttons to sort library by vibe, created function with key data info, added icon

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Access Token 
 const params = new URLSearchParams(window.location.hash);
-const accessToken = params.get("#access_token");
+
+// const accessToken = params.get("#access_token");
+const accessToken = 'BQD26OPk8E6Nh-OQ94fF5r4jKR61Vg9eN5aQUn_jk4OxPnozW_JpOAmLJ682wdNR1c2Hr7muMjb2jWRZh85fNIxaHD1KHXJMWy4fW4-0MQ08eeh3q_r63Ki8yUULayvqlYv2AlNIAoe1A5MLfBgdnnioN736xd521WTCcr1nYudUVDgv9dcVHFQ'
 
 console.log('hi');
 // credentials are optional
@@ -12,7 +14,6 @@ const getUserSavedTracks = async function () {
             'Authorization': 'Bearer ' + accessToken
         }
     });
-
     const json = await response.json()
     return json
 };
@@ -26,13 +27,11 @@ const getTrackItems = async function () {
 console.log(getTrackItems())
 
 
-
 // get track ids 
 const getUserTrackID = async function () {
     const data = await getUserSavedTracks();
     const items = await data.items
     return items.map(item => item.track.id)
-
 };
 console.log(getUserTrackID());
 
@@ -47,13 +46,10 @@ const trackAudioFeat = async function () {
                 'Authorization': 'Bearer ' + accessToken
             }
         });
-
         const json = await response.json();
         dataArray.push(json.energy);
-
     }
     return dataArray
-
 }
 console.log(trackAudioFeat())
 
@@ -61,7 +57,6 @@ const getTrackNames = async function () {
     const data = await getUserSavedTracks();
     const items = await data.items
     return items.map(item => item.track.name)
-
 };
 console.log(getTrackNames())
 
@@ -73,14 +68,12 @@ const getAlbumName = async function () {
 };
 console.log(getAlbumName())
 
-
 const getTrackArtists = async function () {
     const data = await getUserSavedTracks();
     const items = await data.items
     return items.map(item => item.track.artists)
 };
 console.log(getTrackArtists())
-
 
 async function createTrackList() {
     const tracksArr = await getTrackItems();
@@ -102,3 +95,71 @@ async function createTrackList() {
 }
 
 createTrackList();
+
+const getMainData = async function () {
+    const track_items = await getTrackItems();
+    const track_id = await getUserTrackID();
+    const track_energy = await trackAudioFeat();
+    const track_artist = await getTrackArtists()
+    const track_name = await getTrackNames();
+    const track_album = await getAlbumName();
+
+    return {
+        'items': track_items,
+        'track_id': track_id,
+        'track_name': track_name,
+        'track_artist': track_artist,
+        'track_album': track_album,
+        'track_energy': track_energy,
+    }
+
+}
+console.log(getMainData())
+
+// BUTTONS TO SORT SAVED SONGS BY ENERGY
+const lowEnergyButton = document.getElementById('lowEnergy');
+const midEnergyButton = document.getElementById('midEnergy');
+const highEnergyButton = document.getElementById('highEnergy');
+
+// Event listeners for buttons 
+lowEnergyButton.addEventListener('click', async(e) => {
+    e.preventDefault();
+    const data = await getMainData();
+    let lowEnergyData = []
+    for (let i = 0; i < data.items.length; i++) {
+        if (data.track_energy[i].toPrecision(2) <= .33) {
+            lowEnergyData.push(data.track_name[i])
+        }
+    }
+    console.log(lowEnergyData)
+
+
+});
+
+
+midEnergyButton.addEventListener('click', async(e) => {
+    e.preventDefault();
+    const data = await getMainData();
+    let midEnergyData = []
+    for (let i = 0; i < data.items.length; i++) {
+        if (data.track_energy[i].toPrecision(2) > .34 && data.track_energy[i].toPrecision(2) <= .66) {
+            midEnergyData.push(data.track_name[i])
+        }
+    }
+    console.log(midEnergyData)
+
+});
+
+
+highEnergyButton.addEventListener('click', async(e) => {
+    e.preventDefault();
+    const data = await getMainData();
+    let highEnergyData = []
+    for (let i = 0; i < data.items.length; i++) {
+        if (data.track_energy[i].toPrecision(2) > .67 && data.track_energy[i].toPrecision(2) <= 1.0) {
+            highEnergyData.push(data.track_name[i])
+        }
+    }
+    console.log(highEnergyData)
+
+});
