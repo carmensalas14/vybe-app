db9eddc10be7a8bebc2255607af638f00d20ef19
EC2 Default User <ec2-user@ip-172-31-24-236.us-east-2.compute.internal>
2020-03-09 07:46:47 +0000

    added play button to event listeners added create playlist function

--- app.js
+++ app.js
@@ -1,36 +1,21 @@
 // Access Token 
 const params = new URLSearchParams(window.location.hash);
 
-// const accessToken = params.get("#access_token");
-const accessToken = 'BQAAntVCQ10KfQKQevnLh6noyIzfbucs4id8ES6HWkpsx2bLnc6qFGDjUwBeqH6z7OZDxL8T_SLtfYcTT8wi-pJfU-pdoWC0l9J9dbKmWVsMbcgKoQX3ePHc0EOwkP6smNCQ0w8txoyslAbbhxQxxqt6ed2vyE8ZxGXr'
+const accessToken = params.get("#access_token");
+// const accessToken = 'BQDJCW7-86paGLo9Tfz5NMmiqNFNQVuUSXU8b06CNFvNp3geSDRJVtnZgBHbiKtub5_p0esoOizswEMBGuTBY75ElJ-gA5m2WX6LRK_s8DQRGqHm4OWwf8HuYdHMp3G9WDn2Wy-AVUO6AVHg8H7d1e2qV3gZHycGRC90ggraqyYiV1G9Q72BZLs'
 
-// PLAY BUTTON EMBEDED
-window.onload = (e) => {
-    const playButtonDiv = document.getElementById("playButtonDiv")
-    playButtonDiv.innerHTML = `<iframe src="https://open.spotify.com/embed/playlist/2HIgDjIZliTzuhejOsOJGa" width="800" height="580" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
-};
+const playButtonDiv = document.getElementById("playButtonDiv")
 
 // Getting user's Spotify ID
-const getUserID = async function () {
+const getUserId = async function () {
     const response = await fetch('https://api.spotify.com/v1/me', {
         headers: {
             'Authorization': 'Bearer ' + accessToken
         }
     });
     const json = await response.json()
-    return json.id
-};
-// console.log(getUserID())
-
-// Creating playlist with user's ID
-const addDevice = async function (device) {
-    const user_id = await getUserID();
-    const settings = {
-        'method': 'POST',
-        'Content-Type': 'application/json',
-    }
-    const response = await fetch(`https://api.spotify.com/v1/users/${user_id}/playlists`, settings)
-
+    const id = await json.id
+    return id
 };
 
 
@@ -54,24 +39,69 @@ const getTrackItems = async function () {
 // console.log(getTrackItems())
 
 
-// get track ids 
-const getUserTrackID = async function () {
+// get track ID
+const getUserTrackId = async function () {
     const data = await getUserSavedTracks();
     const items = await data.items
     return items.map(item => item.track.id)
 };
-// console.log(getUserTrackID());
+
+// get track URI 
+const getUserTrackURI = async function () {
+    const data = await getUserSavedTracks();
+    const items = await data.items
+    return items.map(item => item.track.uri)
+};
+
+// Generate Playlist function  
+const generatePlaylist = async function (name, trackItems) {
+    //create new playlist for filtered saved tracks
+    const createPlaylist = async function (name) {
+        const user_id = await getUserId();
+        const playlist = await fetch(`https://api.spotify.com/v1/users/${user_id}/playlists`, {
+            method: 'POST',
+            body: JSON.stringify({
+                name: name,
+            }),
+            headers: {
+                'Authorization': 'Bearer ' + accessToken,
+                'Content-Type': 'application/json',
+            }
+        });
+        const emptyPlaylist = await playlist.json()
+        const playlistID = await emptyPlaylist.id
+        return playlistID
+    };
+
+    const playlist_id = await createPlaylist(name);
+    console.log(playlist_id)
+    console.log(trackItems)
+
+    playButtonDiv.innerHTML = `<iframe src="https://open.spotify.com/embed/playlist/${playlist_id}" width="500" height="1000" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+
+    // ADD TRACKS TO PLAYLIST 
+    return fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks`, {
+        method: 'POST',
+        body: JSON.stringify({
+            uris: trackItems
+        }),
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'Content-Type': 'application/json'
+        }
+    });
+}
+generatePlaylist('hi', getUserTrackURI());
 
 
-// get track audio features
+// GET TRACK ENEGRY LEVELS 
 const trackAudioFeat = async function () {
-    const data = await getUserTrackID()
+    const data = await getUserTrackId()
     const dataArray = []
     for (let i = 0; i < data.length; i++) {
         dataArray.push(data[i])
     }
     const dataString = dataArray.join(',')
-    console.log(dataString);
 
     const response = await fetch(`https://api.spotify.com/v1/audio-features/?ids=${dataString}`, {
         headers: {
@@ -81,70 +111,26 @@ const trackAudioFeat = async function () {
     const json = await response.json();
     return json.audio_features.map(track => track.energy)
 }
-console.log(trackAudioFeat())
-
-const getTrackNames = async function () {
-    const data = await getUserSavedTracks();
-    const items = await data.items
-    return items.map(item => item.track.name)
-};
-// console.log(getTrackNames())
-
-const getAlbumName = async function () {
-    const data = await getUserSavedTracks();
-    const items = await data.items
-    return items.map(item => item.track.album.images[1])
-
-};
-// console.log(getAlbumName())
-
-const getTrackArtists = async function () {
-    const data = await getUserSavedTracks();
-    const items = await data.items
-    return items.map(item => item.track.artists)
-};
-// console.log(getTrackArtists())
-
-async function createTrackList() {
-    const tracksArr = await getTrackItems();
-    const trackList = document.createElement('ul')
-    const results = document.getElementById("results")
-
-    function createTrackItem(trackInfo) {
-        const li = document.createElement('li')
-        const artist = trackInfo.track.artists.map(artist => artist.name)
-        li.innerText = `${trackInfo.track.name}, ${artist}`
-        return li
-    }
-
-    for (let i = 0; i < tracksArr.length; i++) {
-        const trackItem = createTrackItem(tracksArr[i])
-        trackList.append(trackItem)
-    }
-    results.appendChild(trackList)
-}
 
 createTrackList();
 
 const getMainData = async function () {
     const track_items = await getTrackItems();
-    const track_id = await getUserTrackID();
+    const track_uri = await getUserTrackURI();
     const track_energy = await trackAudioFeat();
-    const track_artist = await getTrackArtists()
-    const track_name = await getTrackNames();
-    const track_album = await getAlbumName();
 
     return {
         'items': track_items,
-        'track_id': track_id,
-        'track_name': track_name,
-        'track_artist': track_artist,
-        'track_album': track_album,
+        'track_uri': track_uri,
         'track_energy': track_energy,
     }
 
 }
-console.log(getMainData())
+
+// PLAY BUTTON EMBEDED
+window.onload = (e) => {
+    generatePlaylist('Liked Songs♡VYBE♡', getMainData().track_uri)
+};
 
 // BUTTONS TO SORT SAVED SONGS BY ENERGY
 const lowEnergyButton = document.getElementById('lowEnergy');
@@ -154,41 +140,48 @@ const highEnergyButton = document.getElementById('highEnergy');
 // Event listeners for buttons 
 lowEnergyButton.addEventListener('click', async(e) => {
     e.preventDefault();
+    playButtonDiv.innerHTML = ''
+
     const data = await getMainData();
     let lowEnergyData = []
     for (let i = 0; i < data.items.length; i++) {
         if (data.track_energy[i].toPrecision(2) <= .33) {
-            lowEnergyData.push(data.track_id[i])
+            lowEnergyData.push(data.track_uri[i])
         }
     }
     console.log(lowEnergyData)
+    generatePlaylist('LOW♡VYBES', lowEnergyData);
 
 });
 
 
 midEnergyButton.addEventListener('click', async(e) => {
     e.preventDefault();
+    playButtonDiv.innerHTML = ''
+
     const data = await getMainData();
     let midEnergyData = []
     for (let i = 0; i < data.items.length; i++) {
         if (data.track_energy[i].toPrecision(2) > .34 && data.track_energy[i].toPrecision(2) <= .66) {
-            midEnergyData.push(data.track_id[i])
+            midEnergyData.push(data.track_uri[i])
         }
     }
-    console.log(midEnergyData)
+    generatePlaylist('MID♡VYBES', midEnergyData);
 
 });
 
 
 highEnergyButton.addEventListener('click', async(e) => {
     e.preventDefault();
+    playButtonDiv.innerHTML = ''
+
     const data = await getMainData();
     let highEnergyData = []
     for (let i = 0; i < data.items.length; i++) {
         if (data.track_energy[i].toPrecision(2) > .67 && data.track_energy[i].toPrecision(2) <= 1.0) {
-            highEnergyData.push(data.track_id[i])
+            highEnergyData.push(data.track_uri[i])
         }
     }
-    console.log(highEnergyData)
+    generatePlaylist("HIGH♡VYBES", highEnergyData);
 
 });
